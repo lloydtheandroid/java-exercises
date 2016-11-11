4b74d0cacb4d33ac60ea22b6aba205191a23ed5b
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2016-11-11 11:43:36 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by lloyd on 11/11/16.
+ */
+public class HelloWorld {
+        //psvm shortcut
+    public static void main(String[] args) {
+        // same as JS window.console.log()
+        //sout shortcut
+        System.out.println();
+        System.out.print("hello world!!!");
+    }
+}
