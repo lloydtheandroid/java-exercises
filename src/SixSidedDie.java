595ce7f59f8e3af846eb5095a59bda42aaffb764
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2016-11-15 11:39:35 -0600

    Codeup Java exercises: - Calculate Factorial, created a recursion method that outputs a factorial of number between 1 and 19, 20 is too high of a value for an integer. Would need a long to use higher numbers.      example, if entering in "4" will return: 4! = 1 x 2 x 3 x 4   = 24  -  Six Sided Die, created function that generates a random number based on number of sides, returns 2 numbers as if rolling two die.
    TODO:
     - Table of Powers (Not finished);
     - PigLating (Not Finished)

--- src/SixSidedDie.java
+++ src/SixSidedDie.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 /**
  * Created by lloyd on 11/14/16.
  */
 public class SixSidedDie {
     public static void main(String[] args) {
 
+        int NumSides;
+        int DieOne;
+        int DieTwo;
+        String userInput = "N";
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("How many sides does your die have?: ");
+            NumSides = sc.nextInt();
+
+            DieOne = RollSixSidedDie(NumSides);
+            DieTwo = RollSixSidedDie(NumSides);
+
+            System.out.println("Die one is: " + DieOne + " and Die two is: " + DieTwo);
+
+            System.out.print("Do you want to roll the Dice again? ");
+            userInput = sc.next();
+        }
+        while (userInput.equals("y"));
     }
-    private static int RollSixSidedDie() {
-        return (int) Math.ceil(Math.random() * 6);
+    private static int RollSixSidedDie(int NumSides) {
+        return (int) Math.ceil(Math.random() * NumSides);
     }
 }
