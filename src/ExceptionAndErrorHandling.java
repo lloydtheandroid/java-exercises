163bd5db35e11817cb2f46fed331d06504547a6a
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2016-11-16 10:00:41 -0600

    Codeup Java exercises: - Exception Handling exercise, finding a student, (not finished); changed to use different methods for each question instead of one giant method for each. Allows me to use recursive calls when incorrect input is detected. :-)  Going to add more exceptions to the class, but for the most part this does what I want it to do.
    TODO:
     - Table of Powers (Not finished);
     - PigLating (Not Finished)

--- src/ExceptionAndErrorHandling.java
+++ src/ExceptionAndErrorHandling.java
@@ -5,56 +5,59 @@ import java.util.Scanner;
  */
 public class ExceptionAndErrorHandling {
     public static void main(String[] args) {
-        getStudentInfo();
-    }
-    public static void getStudentInfo() {
 
-        String studentName = "";
-        int studentsAge = 0;
-        String studentWhy = "";
-        String userInput;
+        String name = "";
+        int age = 0;
+        String why = "";
+
+
+        System.out.println("Name is " + getStudentName(name));
+        System.out.println("Age is " + getStudentAge(age));
+        System.out.println("Reason for school is " + getStudentWhy(why));
+    }
+    public static String getStudentName(String studentName) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello Welcome to Codeup Student search! \n");
 
-        do {
-            try {
-                System.out.print("What is the student's Name? ");
-                studentName = sc.next();
-            } catch (InputMismatchException e) {
-                sc.next();  // discard the incorrectly entered value
-                System.out.println("Error! Invalid Name. Try again.\n");
-            }
-            System.out.println(); // Blank line for formatting
-
-            try {
-                System.out.print("What is the student's Age? ");
-                studentsAge = sc.nextInt();
-            } catch (InputMismatchException e) {
-                sc.next(); // discard the incorrectly entered value
-                System.out.println("Error! Please enter a whole number. Try again.\n");
-            }
-            System.out.println(); // Blank line for formatting
-
-            try {
-                System.out.println("Why does the student want to learn programming? ");
-                studentWhy = sc.nextLine();
-                if(studentWhy.equals("")) {
-                    System.out.println("Why does the student want to learn programming? ");
-                    studentWhy = sc.nextLine();
-                    throw new IllegalArgumentException("Please enter a valid statement");
-                }
-            } catch (InputMismatchException e) {
-                sc.next(); // discard the incorrectly entered value
-                System.out.println("Error! Please enter a sentence. Try again.\n");
-            }
-            System.out.println(); // Blank line for formatting
-            System.out.println(studentName + " is " + studentsAge + " years old and wants to program because " + studentWhy);
-
-            System.out.print("Do you want to look at another user? \"y/n\"\n");
-            userInput = sc.nextLine();
+        try {
+            System.out.print("What is the student's Name? ");
+            studentName = sc.next();
+        } catch (InputMismatchException e) {
+            sc.next();  // discard the incorrectly entered value
+            System.out.println("Error! Invalid Name. Try again.\n");
         }
-        while (userInput.equalsIgnoreCase("y"));
-        System.out.println("Thank you for using the Student finder. ");
+        System.out.println(); // Blank line for formatting
+        return studentName;
+    }
+    public static int getStudentAge(int studentsAge) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("What is the student's Age? ");
+            studentsAge = sc.nextInt();
+        } catch (InputMismatchException e) {
+            sc.next(); // discard the incorrectly entered value
+            System.out.println("Error! Please enter a whole number. Try again.\n");
+        }
+        System.out.println(); // Blank line for formatting
+        return studentsAge;
+    }
+    public static String getStudentWhy(String studentWhy) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Why does the student want to learn programming? ");
+            studentWhy = sc.nextLine();
+            if (studentWhy.equalsIgnoreCase("")){
+                getStudentWhy(studentWhy);
+            }
+
+        } catch (InputMismatchException e) {
+            sc.next(); // discard the incorrectly entered value
+            System.out.println("Error! Please enter a sentence. Try again.\n");
+        }
+        return studentWhy;
     }
 }
