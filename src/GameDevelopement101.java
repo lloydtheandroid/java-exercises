d83218075c487de552cb8c2bd462b401daa2b83f
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2016-11-18 12:03:47 -0600

    Game Developemt 101, hi or low app create a game that guesses between 1 and 100; added userGuess and GetRandom number;

--- src/GameDevelopement101.java
+++ src/GameDevelopement101.java
@@ -1,5 +1,4 @@
-import com.sun.org.apache.xpath.internal.operations.String;
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -15,27 +14,60 @@ public class GameDevelopement101 {
 //        If user's guess is less than the number, it outputs "HIGHER"
 //        If user's guess is more than the number, it outputs "LOWER"
 //        If a user guesses the number, the game should declare "GOOD GUESS!"
-        Scanner input = new Scanner(System.in);
-        int randomNumber = getRandomNumber();
-//        String highOrLow = isHighOrLow(randomNumber, input);
+        Scanner sc = new Scanner(System.in);
+        java.lang.String userWantsToContinue;
 
-//        System.out.println(highOrLow);
+        do {
+            playHighLowGame();
+            System.out.println("Do you want to play again? y/n");
+            userWantsToContinue = sc.next();
+        } while (userWantsToContinue.equalsIgnoreCase("y") || userWantsToContinue.equalsIgnoreCase("yes"));
 
+        System.out.println("Thank you for playing, yay!");
+    }
+
+    public static void playHighLowGame(){
+        int random;
+        int userGuess;
+
+        random = getRandomNumber();
+
+        do {
+            userGuess = getUserGuess();
+
+            System.out.println("The game guessed " + random);
+
+            if (random > userGuess) {
+                System.out.println("HIGHER!");
+            } else {
+                System.out.println("LOWER!");
+            }
+        }
+        while (random != userGuess);
+        System.out.println("Good Guess!");
     }
     public static int getRandomNumber() {
         return (int) Math.ceil(Math.random() * 100);
     }
-//    public static String isHighOrLow(int randomNumber, Scanner input) {
-//
-//        String highLow;
-//
-//        System.out.println("What is your number? ");
-//        int userGuess = input.nextInt();
-//        if (userGuess < randomNumber) {
-//            highLow += "Too Low";
-//            return highLow;
-//        }
-//        else if (userGuess > randomNumber){
-//        }
-//    }
+
+    public static int getUserGuess() {
+
+        int userInput = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please guess a number betwee 1 and 100: ");
+
+        try {
+            userInput = sc.nextInt();
+            if (userInput < 1 || userInput > 100) {
+                throw new InputMismatchException("Guess must be an integer between 1 and 100");
+            }
+            return userInput;
+        }
+        catch (InputMismatchException e) {
+            System.out.println(e.getMessage());
+            return userInput;
+        }
+
+    }
+
 }
